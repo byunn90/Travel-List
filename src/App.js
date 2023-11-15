67300e77288558b8210cdea0e960992f57f2d653
byunn90 <kayhanturk90@Live.com>
2023-11-15 21:39:45 +1100

    travel list components finished

---
+++ src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🎒</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      🎒 You have X items on your list, and you already packed x (X%)
+    </footer>
+  );
+}
