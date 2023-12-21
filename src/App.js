98bf904342ab4671885aebeedd40a63f6bbee185
byunn90 <kayhanturk90@Live.com>
2023-12-21 14:40:56 +1100

    sorting

--- src/App.js
+++ src/App.js
@@ -79,6 +79,7 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItems }) {
+  const [sortBy, setSortBy] = useState("input");
   return (
     <div className="list">
       <ul className="list">
@@ -91,6 +92,13 @@ function PackingList({ items, onDeleteItem, onToggleItems }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
