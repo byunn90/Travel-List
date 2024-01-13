045b0586b3f8ca3b70a3b0b6368a0a941ec402f3
byunn90 <kayhanturk90@Live.com>
2024-01-13 20:58:49 +1100

    code review and notes

--- src/components/App.js
+++ src/components/App.js
@@ -11,11 +11,13 @@ const initialItems = [
 ];
 
 export default function App() {
+  // here we set a useState for initialItems object of arrays
   const [items, setItems] = useState([initialItems]);
-
+  // Here we have a created a function that stores the new items
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
+  // Function that delete all the items in setItems state then wipes it clear after confirmming it
   function clearItems() {
     const confirmed = window.confirm("Are you sure you want delete all items?");
     if (confirmed) setItems([]);
